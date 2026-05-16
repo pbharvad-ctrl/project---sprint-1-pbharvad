425c63518bc500e8782749086bdb087ef85b2b90
pbharvad-ctrl <pbharvad@depaul.edu>
2026-05-16 18:48:05 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import payment.*;
+
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+
+        PaymentContext context = new PaymentContext();
+
+        context.setPaymentStrategy(new CreditCardPayment());
+        context.executePayment(25.99);
     }
 }
-
-PaymentContext context = new PaymentContext();
-
-context.setPaymentStrategy(new CreditCardPayment());
-context.executePayment(25.99);
