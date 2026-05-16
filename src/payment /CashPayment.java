8eb4248f515055ab42a9575649c0a627e172b43e
pbharvad-ctrl <pbharvad@depaul.edu>
2026-05-16 18:43:45 -0500

    Create CashPayment.java

---
+++ src/payment /CashPayment.java
@@ -0,0 +1,7 @@
+public class CashPayment implements PaymentStrategy {
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Paid $" + amount + " using Cash.");
+    }
+}
