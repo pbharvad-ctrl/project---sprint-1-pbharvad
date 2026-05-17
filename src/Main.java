07f762491f1d0fbae0ba3fdee8c76a1b5796c818
pbharvad-ctrl <pbharvad@depaul.edu>
2026-05-16 19:03:22 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 import payment.*;
+import observer.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
-
+        // Strategy Pattern
         PaymentContext context = new PaymentContext();
 
         context.setPaymentStrategy(new CreditCardPayment());
         context.executePayment(25.99);
 
+        // Observer Pattern
         OrderManager manager = new OrderManager();
         KitchenStaff kitchen = new KitchenStaff();
-        
+
         manager.registerObserver(kitchen);
         manager.placeOrder("Burger with extra cheese");
     }
